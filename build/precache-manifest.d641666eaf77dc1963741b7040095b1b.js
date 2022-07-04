b74a14ac32aa59a480f40d7813ce5e0b8e1378a2
Abhishek Purbey <60577002+abhishek22222@users.noreply.github.com>
2022-07-04 21:50:46 +0530

    files uploaded

---
+++ build/precache-manifest.d641666eaf77dc1963741b7040095b1b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52220cbac0bd74e378b23809a22b264",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f335efcd046ba38aff22",
+    "url": "/static/css/0.10eff1aa.chunk.css"
+  },
+  {
+    "revision": "e5f947a96a4ac8d7bd30",
+    "url": "/static/css/4.6d344725.chunk.css"
+  },
+  {
+    "revision": "6b4e78ae74e673214384",
+    "url": "/static/css/5.b957ddbe.chunk.css"
+  },
+  {
+    "revision": "fc378e5c63302a8b2f17",
+    "url": "/static/css/6.a60aebec.chunk.css"
+  },
+  {
+    "revision": "c0962baa21325c8b367f",
+    "url": "/static/css/7.d37dee14.chunk.css"
+  },
+  {
+    "revision": "b97b82452ac8d44242b2",
+    "url": "/static/css/8.55e8af33.chunk.css"
+  },
+  {
+    "revision": "ad9a50c2a60a4348015d",
+    "url": "/static/css/main.65269f5a.chunk.css"
+  },
+  {
+    "revision": "f335efcd046ba38aff22",
+    "url": "/static/js/0.ab3330ed.chunk.js"
+  },
+  {
+    "revision": "2a6b15382c099562dd5c",
+    "url": "/static/js/3.0d52b6e6.chunk.js"
+  },
+  {
+    "revision": "e5f947a96a4ac8d7bd30",
+    "url": "/static/js/4.3c13b6cf.chunk.js"
+  },
+  {
+    "revision": "6b4e78ae74e673214384",
+    "url": "/static/js/5.fb1850dc.chunk.js"
+  },
+  {
+    "revision": "fc378e5c63302a8b2f17",
+    "url": "/static/js/6.1b534d64.chunk.js"
+  },
+  {
+    "revision": "c0962baa21325c8b367f",
+    "url": "/static/js/7.6d9a4e4e.chunk.js"
+  },
+  {
+    "revision": "b97b82452ac8d44242b2",
+    "url": "/static/js/8.d530b14a.chunk.js"
+  },
+  {
+    "revision": "ad9a50c2a60a4348015d",
+    "url": "/static/js/main.a9cafd01.chunk.js"
+  },
+  {
+    "revision": "f3e4d0cb93864a21cb19",
+    "url": "/static/js/runtime-main.1613bae9.js"
+  }
+]);
